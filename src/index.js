169d547e6350eb3476537507e111fad57d9841e0
limyoursun <limyoursun@gmail.com>
2025-02-20 19:56:48 +0900

    useTabs

--- src/index.js
+++ src/index.js
@@ -1,37 +1,36 @@
-import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
+import React, { useState } from "react";
 
 import "./index.css";
 
-export const useInput = (initialValue, validator) => {
-  const [value, setValue] = useState(initialValue);
-  const onChange = (event) => {
-    const {
-      target: { value },
-    } = event;
-    let willUpdate = true;
-    if (typeof validator === "function") {
-      willUpdate = validator(value);
-    }
-    if (willUpdate) {
-      setValue(value);
-    }
-  };
-  return { value, onChange };
-};
+const useTabs = (initialTab, allTabs) => {
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
+  if(!allTabs || !Array.isArray(allTabs)){
+    return;
+  }
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem : setCurrentIndex
+  }
+}
+
+const content = [
+  {
+    tab:"Section 1",
+    content : "I`m the content of the Section 1"
+  },
+  {
+    tab:"Section 2",
+    content : "I`m the content of the Section 2"
+  }
+]
 
 const App = () => {
-  const maxLength = (value) => !value.includes("@");
-  const name = useInput("Mr.", maxLength);
+  const {currentItem, changeItem} = useTabs(0, content);
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <input
-        type="text"
-        placeholder="Name"
-        value={name.value}
-        onChange={name.onChange}
-      />
+      {content.map((section, index)=> <button onClick={() => {changeItem(index)}}>{section.tab}</button>)}
+      <div>{currentItem.content}</div>
     </div>
   );
 };
