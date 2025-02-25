e459c90e9f08f07661fc7d447d342f4aabf955ca
limyoursun <limyoursun@gmail.com>
2025-02-25 19:38:19 +0900

    useTabs 복습

--- src/index.js
+++ src/index.js
@@ -1,35 +1,32 @@
 import ReactDOM from "react-dom/client";
 import React, { useState } from "react";
 
-import "./index.css";
-
-const useTabs = (initialTab, allTabs) => {
-  const [currentIndex, setCurrentIndex] = useState(initialTab);
-  if(!allTabs || !Array.isArray(allTabs)){
-    return;
-  }
-  return {
-    currentItem: allTabs[currentIndex],
-    changeItem : setCurrentIndex
-  }
-}
-
 const content = [
   {
     tab:"Section 1",
-    content : "I`m the content of the Section 1"
+    content:"this is Section 1"
   },
   {
     tab:"Section 2",
-    content : "I`m the content of the Section 2"
+    content:"this is Section 2"
   }
 ]
+
+const useTabs = (initialTab, allTabs) => {
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
+  return {
+    currentItem : allTabs[currentIndex],
+    changeItem : setCurrentIndex
+  }
+}
 
 const App = () => {
   const {currentItem, changeItem} = useTabs(0, content);
   return (
     <div className="App">
-      {content.map((section, index)=> <button onClick={() => {changeItem(index)}}>{section.tab}</button>)}
+      {content.map((section, index) => (
+        <button onClick={() => changeItem(index)}>{section.tab}</button>
+      ))}
       <div>{currentItem.content}</div>
     </div>
   );
